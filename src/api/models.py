7f7c71d17994382f29d4f958c856899977afb531
JaviJota <165896246+JaviJota@users.noreply.github.com>
2024-07-20 16:22:49 +0000

    commit

--- src/api/models.py
+++ src/api/models.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Date, ForeignKey
 from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -23,6 +24,8 @@ class User(db.Model):
         }
     
 class People(db.Model):
+    __tablename__ = 'people'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=False, nullable=False)
     birth_year = db.Column(db.String(20), unique=False, nullable=False)
@@ -32,4 +35,33 @@ class People(db.Model):
     gender =  db.Column(db.String(80), unique=False, nullable=True)
     hair_color =  db.Column(db.String(80), unique=False, nullable=True)
     skin_color = db.Column(db.String(80), unique=False, nullable=True)
-    planet_id = db.Column(db.Integer, ForeignKey('planets.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+    planet = db.relationship("Planets", back_populates='residents')
+
+    def __repr__(self):
+        return f'<People {self.name}>'
+    
+    def serialize(self):
+        return {
+            'name': self.name,
+            'birth_year' : self.birth_year,
+            'species': self.species,
+            'height': self.height,
+            'mass': self.mass,
+            'gender': self.gender,
+            'hair_color': self.hair_color,
+            'planet': self.planet.name if self.planet else None
+        }
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name =
+    diameter =
+    climate =
+    terrain =
+    population = db.Column(db.Integer, nullable=False)
+    residents = db.relationship('People', back_populates='planet')
+

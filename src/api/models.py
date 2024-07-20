842c2f9cfeea23a17c31dc1d3804a498ed4d4542
JaviJota <165896246+JaviJota@users.noreply.github.com>
2024-07-20 11:14:56 +0000

    Create People and User db Tables

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Date, ForeignKey
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
@@ -6,7 +8,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(50), unique=False, nullable=False)
+    last_name = db.Column(db.String(100), unique=False, nullable=False)
+    creation_date = db.Column(db.Date, default=func.current_date(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,3 +21,15 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    birth_year = db.Column(db.String(20), unique=False, nullable=False)
+    species = db.Column(db.String(80), unique=False, nullable=False)
+    height =  db.Column(db.Integer, unique=False, nullable=False)
+    mass =  db.Column(db.Integer, unique=False, nullable=False)
+    gender =  db.Column(db.String(80), unique=False, nullable=True)
+    hair_color =  db.Column(db.String(80), unique=False, nullable=True)
+    skin_color = db.Column(db.String(80), unique=False, nullable=True)
+    planet_id = db.Column(db.Integer, ForeignKey('planets.id'))
